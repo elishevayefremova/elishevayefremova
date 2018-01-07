a5a6b0f9398d44cee8ab448ca6e3ad7eb0d849d1
elishevayefremova <elisheva.yefremova@gmail.com>
2018-01-07 20:06:21 +0200

    task

--- task/js/scripts.js
+++ task/js/scripts.js
@@ -1,5 +1,41 @@
 (function($){
     $(document).ready(function () {
-		//
-    })
+		var menu = $('.menu');
+			items = $('.menu li');
+
+		$(menu).on('click', function(e){
+			e.preventDefault();
+			items.slideToggle();
+		});
+
+
+		$('.items').slick({
+			  infinite: true,
+			  speed: 300,
+			  slidesToShow: 5,
+			  slidesToScroll: 1,
+			  arrows: true,
+			  responsive: [
+			    {
+			      breakpoint: 900,
+			      settings: {
+			        slidesToShow: 3,
+			        slidesToScroll: 1,
+			        infinite: true,
+			      }
+			    },
+			    {
+			      breakpoint: 500,
+			      settings: {
+			        slidesToShow: 1,
+			        slidesToScroll: 1,
+			        arrows: false,
+			      }
+			    },
+			  ],
+
+			});
+    });
+
+
 })(jQuery);

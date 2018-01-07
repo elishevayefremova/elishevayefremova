f6ad6bfcd010c8afe592a27745239ad39b3ab016
elishevayefremova <elisheva.yefremova@gmail.com>
2018-01-07 17:07:04 +0200

    Update scripts.js

--- task/js/scripts.js
+++ task/js/scripts.js
@@ -1,5 +1,11 @@
 (function($){
     $(document).ready(function () {
-		//
+		var menu = $('.menu');
+			items = $('.menu li');
+
+		$(menu).on('click', function(e){
+			e.preventDefault();
+			items.slideToggle();
+		});
     })
 })(jQuery);

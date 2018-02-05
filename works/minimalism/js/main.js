cb6ce6d2cb48ed374d3d49418a31938c8cdbac8b
Elisheva <elisheva.yefremova@gmail.com>
2018-02-05 11:46:55 +0200

    minimalism

---
+++ works/minimalism/js/main.js
@@ -0,0 +1,39 @@
+new WOW().init();
+
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() >= 80) {
+//        $('.first').css({
+//             'display': 'none'
+
+//         });
+//        // $('.first .left a').removeClass('wow');
+//        $('.second').css({
+//             'display': 'block'
+//         });
+//        $('.second').addClass('wow');
+//     }
+//     if ($(this).scrollTop() < 80) {
+//         $('.second').css({
+//             'display': 'none'
+//         });
+//         $('.second').removeClass('wow');
+//         $('.first').css({
+//             'display': 'block'
+//         });
+//         $('.first .left a').addClass('wow');
+//         $('.first .left a').addClass('slideInUp');
+//     }
+// });
+
+// $(window).scroll(function() {
+//     if ($('.first').css('display')=='none'){
+//     	$('.second').css({
+//             'display': 'block'
+//         });
+//     }
+//      if ($('.second').css('display')=='none'){
+//     	$('.first').css({
+//             'display': 'block'
+//         });
+//     }
+// });

4cb8359a33a6ef9409f645c3c2ab5b0deb6cb0df
Elisheva <elisheva.yefremova@gmail.com>
2018-02-06 13:22:59 +0200

    min

--- works/minimalism/js/main.js
+++ works/minimalism/js/main.js
@@ -3,29 +3,44 @@ new WOW().init();
 $( document ).ready(function() {
     console.log( "ready!" );
       
-	$("body").niceScroll({
-		scrollspeed: 20, // scrolling speed
+	$('body').niceScroll({
+		scrollspeed: 5, // scrolling speed
     	mousescrollstep: 20,
 	});
 	
 
-    $('body').mousewheel(function(event) {
-   		var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+   //  $('body').mousewheel(function(event) {
+   // 		var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
 
-   		if ((event.deltaY<0)&&(scrollBottom<=0.75)&&($('section').hasClass('second-unvisible'))) {
-   			setTimeout(1000);
-    		new WOW().init();
-	       $('.first').addClass('first-unvisible');
-	       $('.second').removeClass('second-unvisible');
-	       setTimeout(1000);
+   // 		if ((event.deltaY<0)&&(scrollBottom<=0.75)&&($('section').hasClass('second-unvisible'))) {
+   // 			setTimeout(1000);
+   //  		new WOW().init();
+	  //      $('.first').addClass('first-unvisible');
+	  //      $('.second').removeClass('second-unvisible');
+	  //      setTimeout(1000);
 
-	    }
-	    if ((event.deltaY>0)&&($('section').hasClass('first-unvisible'))) {
-    		new WOW().init();
+	  //   }
+	  //   if ((event.deltaY>0)&&($('section').hasClass('first-unvisible'))) {
+   //  		new WOW().init();
+	  //      $('.first').removeClass('first-unvisible');
+	  //      $('.second').addClass('second-unvisible');
+	  //      setTimeout(100);
+	  //   }
+  	// });
+   
+  	 $('.second').click(function(){
+  	 	   new WOW().init();
 	       $('.first').removeClass('first-unvisible');
 	       $('.second').addClass('second-unvisible');
 	       setTimeout(100);
-	    }
-  });  
+
+  	 });
+  	 $('.button').click(function(){
+  	 	   new WOW().init();
+	       $('.first').addClass('first-unvisible');
+	       $('.second').removeClass('second-unvisible');
+	       setTimeout(100);
+
+  	 });
     
 });
